c600ec3f7688397204c59342bcf7f647324d16f3
Michael Wieckowski <mike@heatworks.tech>
2018-02-09 15:13:06 -0500

    Functional microchip BSP net implementation.  Removed TLS from Xivley side.

--- src/bsp/platform/microchip_incomplete/xi_bsp_io_net_microchip.c
+++ src/bsp/platform/microchip_incomplete/xi_bsp_io_net_microchip.c
@@ -53,10 +53,10 @@ extern "C" {
 xi_bsp_io_net_state_t xi_bsp_io_net_create_socket( xi_bsp_socket_t* xi_socket )
 {
     //( void )xi_socket;    
-    xi_socket = socket(AF_INET, SOCK_STREAM, SOCKET_FLAGS_SSL);
-    if (xi_socket < 0) {
+    *xi_socket = socket(AF_INET, SOCK_STREAM, SOCKET_FLAGS_SSL);
+    if (*xi_socket < 0) {
 		SYS_CONSOLE_PRINT("Socket creation error\r\n");
-		close(xi_socket);
+		close(*xi_socket);
 		return XI_BSP_IO_NET_STATE_ERROR;
 	}
     return XI_BSP_IO_NET_STATE_OK;
@@ -72,19 +72,19 @@ xi_bsp_io_net_connect( xi_bsp_socket_t* xi_socket, const char* host, uint16_t po
 	addr_in.sin_addr.s_addr = mqttData.resolvedMqttIp.Val;
 
 	/* Create secure socket. */
-	if (xi_socket < 0) {
-		xi_socket = socket(AF_INET, SOCK_STREAM, SOCKET_FLAGS_SSL);
+	if (*xi_socket < 0) {
+		*xi_socket = socket(AF_INET, SOCK_STREAM, SOCKET_FLAGS_SSL);
 	}
 
 	/* Check if socket was created successfully. */
-	if (xi_socket < 0) {
+	if (*xi_socket < 0) {
 		SYS_CONSOLE_PRINT("Socket creation error\r\n");
-		close(xi_socket);
+		close(*xi_socket);
 		return XI_BSP_IO_NET_STATE_ERROR;
 	}
-
+    SYS_CONSOLE_PRINT("Xively socket %d created.\r\n", *xi_socket);
 	/* If success, connect to socket. */
-	if (connect(xi_socket, (struct sockaddr *)&addr_in, sizeof(struct sockaddr_in)) != SOCK_ERR_NO_ERROR) {
+	if (connect(*xi_socket, (struct sockaddr *)&addr_in, sizeof(struct sockaddr_in)) != SOCK_ERR_NO_ERROR) {
 		SYS_CONSOLE_PRINT("Socket connection error\r\n");
 		return XI_BSP_IO_NET_STATE_ERROR;
 	}
@@ -124,29 +124,22 @@ xi_bsp_io_net_state_t xi_bsp_io_net_write( xi_bsp_socket_t xi_socket,
 
         return XI_BSP_IO_NET_STATE_CONNECTION_RESET;
     }
-    //TO DO:  MJW how do we check if the winc1500 is "Ready" to receive more data?
-    /*else if ( TCPIP_TCP_PutIsReady( xi_socket ) == 0)
-    {
-        //MJW changed b/c XI_BSP_IO_NET_STATE_WANT_WRITE doesn't exist??
-        //return XI_BSP_IO_NET_STATE_WANT_WRITE;        
-        return XI_BSP_IO_NET_STATE_BUSY;
-    }*/
-        
-    send((SOCKET) xi_socket, buf, count, 0);
     
-    //looks like "out written count" is used to monitor what has been sent.  We only know this from our socket callback.
-    // how to deal with this?
-    *out_written_count = wifiData.s16Sent;
-    SYS_CONSOLE_PRINT( "written: %d bytes\r\n", *out_written_count );
-
-    if ( 0 == *out_written_count )
-    {
-        /* treat this like EAGAIN */
-        //MJW changed b/c XI_BSP_IO_NET_STATE_WANT_WRITE doesn't exist??
-        //return XI_BSP_IO_NET_STATE_WANT_WRITE;
-        SYS_CONSOLE_PRINT( "Wanted to write, but couldn't!\r\n");
+    // if the socket is ready, we will give it a request to send data.  The data is NOT sent immediately.
+    //so, we return "zero bytes sent, NET State Busy."
+    //This will result in a future call to this function when we must NOT call "send" to the socket, and instead, return how many bytes have been transmitted.
+    if(wifiData.socketTxReady){
+        send((SOCKET) xi_socket, buf, count, 0);
+        *out_written_count = 0;
+        wifiData.socketTxReady = false;
+        wifiData.socketTxCompleted = false;
         return XI_BSP_IO_NET_STATE_BUSY;
-    }
+    }    
+    else if(wifiData.socketTxCompleted){    //we're not ready, but TX completed.  This means the full transaction has taken place.
+        *out_written_count = wifiData.socketNumBytesSent;
+        wifiData.socketTxReady = true;
+        SYS_CONSOLE_PRINT( "Xively wrote: %d bytes\r\n", *out_written_count );
+    }       
 
     return XI_BSP_IO_NET_STATE_OK;
 }
@@ -161,34 +154,19 @@ xi_bsp_io_net_state_t xi_bsp_io_net_read( xi_bsp_socket_t xi_socket,
         SYS_CONSOLE_PRINT( "connection reset by peer\r\n" );
         return XI_BSP_IO_NET_STATE_CONNECTION_RESET;
     }
-
-    recv((SOCKET) xi_socket, buf, count, 10000);    //10 second timeout??  what should we use here
     
-    *out_read_count = wifiData.s16Received;
-    
-    SYS_CONSOLE_PRINT( "read: %d bytes\r\n", *out_read_count );
-
-    if ( *out_read_count < 0 )
-    {
-        /*if ( TCPIP_TCP_WasReset( xi_socket ) || !TCPIP_TCP_IsConnected( xi_socket ) )
-        {
-            SYS_CONSOLE_PRINT( "connection reset by peer\r\n" );
-            return XI_BSP_IO_NET_STATE_CONNECTION_RESET;
-        }
-        else
-        {
-            //MJW changed b/c XI_BSP_IO_NET_STATE_WANT_READ doesn't exist??
-            //return XI_BSP_IO_NET_STATE_WANT_READ;
-            return XI_BSP_IO_NET_STATE_BUSY;            
-        }*/
-        return XI_BSP_IO_NET_STATE_BUSY;            
-    }
-    else if ( *out_read_count == 0 )
-    {
-        /* treat this like EAGAIN */
-        //MJW changed b/c XI_BSP_IO_NET_STATE_WANT_READ doesn't exist??
-        //return XI_BSP_IO_NET_STATE_WANT_READ;
-        return XI_BSP_IO_NET_STATE_BUSY;            
+    //same logic as TX
+    if(wifiData.socketRxReady){
+        recv((SOCKET) xi_socket, buf, count, 10000);    //10 second timeout??  what should we use here
+        *out_read_count = 0;
+        wifiData.socketRxReady = false;
+        wifiData.socketRxCompleted = false;
+        return XI_BSP_IO_NET_STATE_BUSY;
+    }    
+    else if(wifiData.socketRxCompleted){    //we're not ready, but TX completed.  This means the full transaction has taken place.
+        *out_read_count = wifiData.socketNumBytesReceived;
+        wifiData.socketRxReady = true;
+        SYS_CONSOLE_PRINT( "Xively read: %d bytes\r\n", *out_read_count );
     }
 
     return XI_BSP_IO_NET_STATE_OK;
@@ -207,38 +185,42 @@ xi_bsp_io_net_state_t xi_bsp_io_net_select( xi_bsp_socket_events_t* socket_event
 {
     /* unused at least for now */
     ( void )timeout;
-
+        
     /* translate the library socket events settings to the set's of events used by posix
      * select */
     size_t socket_id = 0;
+    //MJW To Do:  will xively ever open more than 1 socket?  Seems like this is implied here, but our main wifi.c loop assumes a single socket connection.
     for ( socket_id = 0; socket_id < socket_events_array_size; ++socket_id )
     {
         xi_bsp_socket_events_t* socket_events = &socket_events_array[socket_id];
 
         if ( 1 == socket_events->in_socket_want_connect )
-        {
-            /*if ( TRUE == TCPIP_TCP_IsConnected( socket_events->xi_socket ) )
+        {            
+            //if ( TRUE == TCPIP_TCP_IsConnected( socket_events->xi_socket ) )
+            if(wifiData.socketStatus == SOCKET_CONNECTED)
             {
                 socket_events->out_socket_connect_finished = 1;
-            }*/
+            }
             continue;
         }
 
-        /*if ( 1 == socket_events->in_socket_want_read &&
-             ( TCPIP_TCP_GetIsReady( socket_events->xi_socket ) > 0 ||
-               0 == TCPIP_TCP_IsConnected( socket_events->xi_socket ) ) )
+//        if ( 1 == socket_events->in_socket_want_read &&
+//             ( TCPIP_TCP_GetIsReady( socket_events->xi_socket ) > 0 ||
+//               0 == TCPIP_TCP_IsConnected( socket_events->xi_socket ) ) )
+        if((1 == socket_events->in_socket_want_read) && (wifiData.socketStatus == SOCKET_CONNECTED))
         {
             socket_events->out_socket_can_read = 1;
             continue;
         }
 
-        if ( 1 == socket_events->in_socket_want_write &&
-             ( TCPIP_TCP_PutIsReady( socket_events->xi_socket ) > 0 ||
-               0 == TCPIP_TCP_IsConnected( socket_events->xi_socket ) ) )
+//        if ( 1 == socket_events->in_socket_want_write &&
+//             ( TCPIP_TCP_PutIsReady( socket_events->xi_socket ) > 0 ||
+//               0 == TCPIP_TCP_IsConnected( socket_events->xi_socket ) ) )
+        if((1 == socket_events->in_socket_want_write) && (wifiData.socketStatus == SOCKET_CONNECTED))
         {
             socket_events->out_socket_can_write = 1;
             continue;
-        }*/
+        }
 
         if ( 1 == socket_events->in_socket_want_error )
         {
